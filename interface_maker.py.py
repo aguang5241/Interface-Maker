5e30827d51c197aa56a47784679093c3a045d048
aguang5241 <gliu4@wpi.edu>
2024-10-08 14:07:18 -0400

    modified:   interface_maker.py.py renamed:    output/interfaces/intf_0_001_001.vasp -> output/interfaces/intf_1_001_001.vasp renamed:    output/interfaces/intf_0_001_001/slab_0_001.vasp -> output/interfaces/intf_1_001_001/intf_1_slab_0_001.vasp renamed:    output/interfaces/intf_0_001_001/slab_1_001.vasp -> output/interfaces/intf_1_001_001/intf_1_slab_1_001.vasp renamed:    output/interfaces/intf_1_010_111.vasp -> output/interfaces/intf_2_010_111.vasp renamed:    output/interfaces/intf_1_010_111/slab_0_010.vasp -> output/interfaces/intf_2_010_111/intf_2_slab_0_010.vasp renamed:    output/interfaces/intf_1_010_111/slab_1_111.vasp -> output/interfaces/intf_2_010_111/intf_2_slab_1_111.vasp modified:   output/interfaces/intf_profile.csv

--- interface_maker.py.py
+++ interface_maker.py.py
@@ -110,10 +110,10 @@ def gen_intf(i, profile):
     slab_1 = make_supercell(slab_1, T_1, order='atom-major')
 
     # Write the transformed slab data
-    save_path = f'output/interfaces/intf_{i}_{hkl_0}_{hkl_1}'
+    save_path = f'output/interfaces/intf_{i+1}_{hkl_0}_{hkl_1}'
     os.makedirs(save_path)
-    write(f'{save_path}/slab_0_{hkl_0}.vasp', slab_0, format='vasp', direct=False)
-    write(f'{save_path}/slab_1_{hkl_1}.vasp', slab_1, format='vasp', direct=False)
+    write(f'{save_path}/intf_{i+1}_slab_0_{hkl_0}.vasp', slab_0, format='vasp', direct=False)
+    write(f'{save_path}/intf_{i+1}_slab_1_{hkl_1}.vasp', slab_1, format='vasp', direct=False)
 
     # Compare the areas of slab_0 and slab_1, let slab_0 be the lower slab with the larger area
     area_0, area_1 = profile[5], profile[6]
@@ -170,7 +170,7 @@ def gen_intf(i, profile):
         interface.positions[:, 2] = interface_thickness-interface.positions[:, 2]
     
     # Write the interface
-    write(f'output/interfaces/intf_{i}_{hkl_0}_{hkl_1}.vasp', interface, format='vasp', direct=False)
+    write(f'output/interfaces/intf_{i+1}_{hkl_0}_{hkl_1}.vasp', interface, format='vasp', direct=False)
 
     # Store the lattice matching data
     with open('output/interfaces/intf_profile.txt', 'a') as f:
@@ -199,7 +199,7 @@ def gen_intf(i, profile):
     
     # Store the lattice matching data in a csv file
     with open('output/interfaces/intf_profile.csv', 'a') as f:
-        f.write(f'{i},{len(interface)},{str(hkl_0)},{str(hkl_1)},{area_0:.6f},{area_1:.6f},{profile[2]*100:.6f},{profile[3]*100:.6f},{profile[4]:.6f},{np.abs(area_0-area_1)/area_0*100:.6f},{T_0[0][0]:.6f},{T_0[0][1]:.6f},{T_0[1][0]:.6f},{T_0[1][1]:.6f},{T_1[0][0]:.6f},{T_1[0][1]:.6f},{T_1[1][0]:.6f},{T_1[1][1]:.6f}\n')
+        f.write(f'{i+1},{len(interface)},{str(hkl_0)},{str(hkl_1)},{area_0:.6f},{area_1:.6f},{profile[2]*100:.6f},{profile[3]*100:.6f},{profile[4]:.6f},{np.abs(area_0-area_1)/area_0*100:.6f},{T_0[0][0]:.6f},{T_0[0][1]:.6f},{T_0[1][0]:.6f},{T_0[1][1]:.6f},{T_1[0][0]:.6f},{T_1[0][1]:.6f},{T_1[1][0]:.6f},{T_1[1][1]:.6f}\n')
 
 def trim(data):
     data = np.array(data)
